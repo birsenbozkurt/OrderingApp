ab50d5f490304e313baff9c65803029b278a2ee3
birsenbozkurt <birsenbozkurt4041@gmail.com>
2022-09-19 17:49:16 +0300

    detail page design

--- lib/views/food_detail_page.dart
+++ lib/views/food_detail_page.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_firs
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ordering_app/cubits/basketpage_cubit.dart';
@@ -31,25 +32,58 @@ class _FoodDetailPageState extends State<FoodDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Yemek Detay", style: TextStyle(fontSize: 23, fontStyle: FontStyle.italic)),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new_sharp),
+            color: Colors.white,
+            iconSize: 30,
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        title: const Text("Yemek Detay", style: TextStyle(fontSize: 23, color: Colors.white, fontStyle: FontStyle.italic)),
         centerTitle: true,
         backgroundColor: Colors.orange,
       ),
       body: Center(
         child: Column(
           children: [
+            Container(
+              height: 20,
+            ),
+            Card(
+                color: Colors.grey.shade300,
+                shape: BeveledRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    side: BorderSide(
+                      color: Colors.orange.shade700,
+                      width: 2.0,
+                    )),
+                child: Image.network(width: 200, height: 200, "http://kasimadalan.pe.hu/yemekler/resimler/${widget.yemek.yemek_resim_adi}")),
+            Container(
+              height: 10,
+            ),
+            Text(
+              "${widget.yemek.yemek_adi}",
+              style: TextStyle(fontSize: 25),
+            ),
             Container(),
-            Image.network(width: 200, height: 200, "http://kasimadalan.pe.hu/yemekler/resimler/${widget.yemek.yemek_resim_adi}"),
-            Container(),
-            Text("${widget.yemek.yemek_adi}"),
-            Container(),
-            Text("${widget.yemek.yemek_fiyat} ₺"),
-            Container(),
+            Text(
+              "${widget.yemek.yemek_fiyat} ₺",
+              style: TextStyle(fontSize: 35),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 30, bottom: 30),
+              child: Container(
+                child: ElevatedButton.icon(onPressed: () {}, icon: Icon(Icons.download_rounded), label: Text("Adet Seçiniz")),
+                height: 50,
+                width: 250,
+              ),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 IconButton(
                   iconSize: 35,
+                  color: Colors.orange.shade700,
                   onPressed: () {
                     setState(() {
                       if (yemek_siparis_adet == 1) {
@@ -58,39 +92,25 @@ class _FoodDetailPageState extends State<FoodDetailPage> {
                       yemek_siparis_adet--;
                     });
                   },
-                  icon: Icon(Icons.remove_circle_outline),
+                  icon: Icon(Icons.arrow_circle_down_outlined),
                 ),
-                Text("${yemek_siparis_adet}"),
+                Text(
+                  "${yemek_siparis_adet}",
+                  style: TextStyle(fontSize: 30),
+                ),
                 IconButton(
+                  iconSize: 35,
+                  color: Colors.orange.shade700,
                   onPressed: (() {
                     setState(() {
                       yemek_siparis_adet++;
                     });
                   }),
-                  icon: Icon(Icons.add_circle_outline_outlined),
+                  icon: Icon(Icons.arrow_circle_up_outlined),
                 ),
               ],
             ),
             Container(),
-            // BlocBuilder<BasketPageCubit, List<BasketFoods>>(builder: ((context, yemeklerListesi) {
-            //   return ElevatedButton(
-            //       onPressed: () {
-            //         for (var i = 0; i < yemeklerListesi.length; i++) {
-            //           if (widget.yemek.yemek_adi == yemeklerListesi[i].yemek_adi) {
-            //             click = 0;
-            //             ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-            //               duration: Duration(seconds: 1),
-            //               content: Text("Ürün Sepette"),
-            //             ));
-            //             return;
-            //           }
-            //         }
-            //         if(click==1){
-            //           context.read<FoodDetailCubit>().
-            //         }
-            //       },
-            //       child: child);
-            // }))
           ],
         ),
       ),
